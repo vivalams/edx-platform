a71f006962fedab8afe24702f818e82400a7c84d
Kabir Khan <kakh@microsoft.com>
2017-10-18 15:55:18 -0700

    adding v1 urls to lms

--- lms/djangoapps/grades/api/v1/urls.py
+++ lms/djangoapps/grades/api/v1/urls.py
@@ -8,17 +8,17 @@ from lms.djangoapps.grades.api.views import CourseGradingPolicy
 urlpatterns = patterns(
     '',
     url(
-        r'^v1/course_grade/{course_id}/users/$'.format(
+        r'^course_grade/{course_id}/users/$'.format(
             course_id=settings.COURSE_ID_PATTERN,
         ),
         views.CourseGradeView.as_view(), name='course_grade_detail'
     ),
     url(
-        r'^v1/user_grades/$',
+        r'^user_grades/$',
         views.UserGradeView.as_view(), name='user_grade_detail'
     ),
     url(
-        r'^v1/courses/{course_id}/policy/$'.format(
+        r'^courses/{course_id}/policy/$'.format(
             course_id=settings.COURSE_ID_PATTERN,
         ),
         CourseGradingPolicy.as_view(), name='course_grading_policy'
