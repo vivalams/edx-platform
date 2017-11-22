b099d26cf37e2aa49df66d56cda840d0f00ef9f3
Manikarthik Konakalla <v-mankon@microsoft.com>
2017-11-22 13:57:35 -0800

    Multiple site organizations  fix (#141)
    * fixing conflicts
    
    * Adding line at end of the file

--- openedx/core/djangoapps/site_configuration/helpers.py
+++ openedx/core/djangoapps/site_configuration/helpers.py
@@ -244,12 +244,11 @@ def get_lms_base_values(org, default=None):
             site_config = SiteConfiguration.objects.get(site=site)
         except:
             return default
-        course_org_filter = site_config.get_value('course_org_filter')
-
-        if course_org_filter and org in course_org_filter:
-            lms_base_value = site_config.get_value('LMS_BASE')
-            if site.name not in site_dict:
-                site_dict[site.name] = lms_base_value
+        if site_config.get_value('course_org_filter'):
+            if org in site_config.get_value('course_org_filter'):
+                lms_base_value = site_config.get_value('LMS_BASE')
+                if site.name not in site_dict:
+                    site_dict[site.name] = lms_base_value
         else:
             return default
     return site_dict
