57b62757e63f7cacd22a09a8125da801cd8998a3
Stephen Dolenc <stepdo@microsoft.com>
2017-12-10 13:06:56 -0800

    all mult-site & single-course work (thus far)
    (cherry picked from commit 509a12594b0c862862a8c7fa779a568e288d284f)

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -1242,3 +1242,6 @@ RETRY_ACTIVATION_EMAIL_TIMEOUT = 0.5
 
 # How long until database records about the outcome of a task and its artifacts get deleted?
 USER_TASKS_MAX_AGE = timedelta(days=7)
+
+# this setting will enable dropdown with site names, used in multi sites with multiple org scenarios
+ENABLE_MULTI_SITE_VIEWLIVE_DROPDOWN = False
