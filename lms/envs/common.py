96e027d553880f6081b00948f18db9e3cbbf1fa4
Kabir Khan <kabirkhan14@MacBook-Pro-3.local>
2018-04-04 16:30:49 -0600

    Removing flag for bulk grades api

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -424,10 +424,6 @@ FEATURES = {
     # Whether to send an email for failed password reset attempts or not. This is mainly useful for notifying users
     # that they don't have an account associated with email addresses they believe they've registered with.
     'ENABLE_PASSWORD_RESET_FAILURE_EMAIL': False,
-
-    # Whether to enable v1 grades api with bulk pull functionality
-    # (Staff user can pull all grades for users in a course.)
-    'ENABLE_BULK_GRADES_API': False,
 }
 
 # Settings for the course reviews tool template and identification key, set either to None to disable course reviews
