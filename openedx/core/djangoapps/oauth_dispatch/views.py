0591f0b99be553b679baec2258fd1d269976259f
SathishKumar Eswaran <seswar@microsoft.com>
2018-04-05 15:39:38 -0700

    changes models

--- openedx/core/djangoapps/oauth_dispatch/views.py
+++ openedx/core/djangoapps/oauth_dispatch/views.py
@@ -7,7 +7,6 @@ from __future__ import unicode_literals
 
 import hashlib
 import json
-import pdb
 from Cryptodome.PublicKey import RSA
 from django.conf import settings
 from django.core.urlresolvers import reverse
@@ -22,7 +21,7 @@ from ratelimit.mixins import RatelimitMixin
 
 from openedx.core.djangoapps.auth_exchange import views as auth_exchange_views
 from openedx.core.lib.token_utils import JwtBuilder
-from openedx.core.djangoapps.oauth_dispatch.models import RestrictedApplication 
+from openedx.core.djangoapps.oauth_dispatch.models import RestrictedApplication
 from . import adapters
 
 
@@ -84,15 +83,39 @@ class _DispatchingView(View):
         else:
             return request.POST.get('client_id')
 
+    def _get_application_id(self, request):
+        """
+        Return application id from provided request
+        """
+        return dot_models.Application.objects.get(client_id=self._get_client_id(request)).id
+
     def get_auth_type(self, request):
         """
         Returns the appropriate adapter based on the OAuth client linked to the request.
         """
-        dot_id = dot_models.Application.objects.get(client_id=self._get_client_id(request)).id
+        dot_id = self._get_application_id(request)
+        #dot_models.Application.objects.filter(client_id__contains=self._get_client_id(request))
         if RestrictedApplication.objects.filter(application_id=dot_id).exists():
-            return 'oauth2'
+            return True
         else:
-            return 'jwt'
+            return False
+
+    def get_associated_org(self, request):
+        """
+        Returns the appropriate adapter based on the OAuth client linked to the request.
+        """
+        dot_id = self._get_application_id(request)
+        if RestrictedApplication.objects.filter(application_id=dot_id).exists():
+            return RestrictedApplication.objects.get(application_id=dot_id).org_associations
+        else:
+            return None
+
+    def get_grant_type(self, request):
+        """
+        Returns grant type of the application
+        """
+        return dot_models.Application.objects.get(client_id=self._get_client_id(request)).authorization_grant_type
+
 
 class AccessTokenView(RatelimitMixin, _DispatchingView):
     """
@@ -107,13 +130,17 @@ class AccessTokenView(RatelimitMixin, _DispatchingView):
 
     def dispatch(self, request, *args, **kwargs):
         response = super(AccessTokenView, self).dispatch(request, *args, **kwargs)
-        auth_type = self.get_auth_type(request)
-        #pdb.set_trace()
+
         if response.status_code == 200 and request.POST.get('token_type', '').lower() == 'jwt':
             expires_in, scopes, user = self._decompose_access_token_response(request, response)
-            print(auth_type) 
+            auth_type = self.get_auth_type(request)
+            if auth_type:
+                org = self.get_associated_org(request)
+            else:
+                org = None
+            grant_type = self.get_grant_type(request)
             content = {
-                'access_token': JwtBuilder(user, auth_type=auth_type).build_token(scopes, expires_in),
+                'access_token': JwtBuilder(user, auth_type=auth_type).build_token(scopes, expires_in, org=org, grant_type=grant_type),
                 'expires_in': expires_in,
                 'token_type': 'JWT',
                 'scope': ' '.join(scopes),
@@ -125,7 +152,6 @@ class AccessTokenView(RatelimitMixin, _DispatchingView):
     def _decompose_access_token_response(self, request, response):
         """ Decomposes the access token in the request to an expiration date, scopes, and User. """
         content = json.loads(response.content)
-        print(content)
         access_token = content['access_token']
         scope = content['scope']
         access_token_obj = self.get_adapter(request).get_access_token(access_token)
