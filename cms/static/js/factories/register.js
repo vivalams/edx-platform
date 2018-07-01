c5e986d0e3e4b43939ad2d4afa5aba7502824c0d
Prabhanshu Pandit <prabhanshupandit.pu@gmail.com>
2018-07-01 00:27:52 -0700

    inplace edit for aad

--- cms/static/js/factories/register.js
+++ cms/static/js/factories/register.js
@@ -1,6 +1,7 @@
 define(['jquery', 'jquery.cookie'], function($) {
     'use strict';
-    return function() {
+    return function(options) {
+        var $registerForm = $('form#register_form');
         $('form :input')
             .focus(function() {
                 $('label[for="' + this.id + '"]').addClass('is-focused');
@@ -9,7 +10,7 @@ define(['jquery', 'jquery.cookie'], function($) {
                 $('label').removeClass('is-focused');
             });
 
-        $('form#register_form').submit(function(event) {
+        $('#register_form').submit(function(event) {
             event.preventDefault();
             var submit_data = $('#register_form').serialize();
 
@@ -21,13 +22,23 @@ define(['jquery', 'jquery.cookie'], function($) {
                 notifyOnError: false,
                 data: submit_data,
                 success: function(json) {
-                    location.href = '/course/';
+                    if (json.redirect_url) {
+                        location.href = json.redirect_url;
+                    } else {
+                        location.href = '/course/';
+                    }
                 },
                 error: function(jqXHR, textStatus, errorThrown) {
                     var json = $.parseJSON(jqXHR.responseText);
+                    $('body').show();
                     $('#register_error').html(json.value).stop().addClass('is-shown');
                 }
             });
         });
+
+        if (options.autoSubmitRegForm) {
+            $('body').hide();
+            $registerForm.submit();
+        }
     };
 });
