10cee3d8d00ff8b71ac434ab982982f590115285
Prabhanshu Pandit <prabhanshupandit.pu@gmail.com>
2018-09-11 22:57:48 +0530

    error msg fix

--- lms/static/js/student_account/views/RegisterView.js
+++ lms/static/js/student_account/views/RegisterView.js
@@ -142,7 +142,13 @@
                                 function(errorList) {
                                     return _.map(
                                         errorList,
-                                        function(errorItem) { return '<li>' + errorItem.user_message + '</li>'; }
+                                        function(errorItem) {
+                                            if (errorItem.user_message === 'Full Name'){
+                                                let msgPart1 = gettext("Please ensure your Microsoft account contains both a first name and last name on your <a href='https://account.microsoft.com'>account settings</a> prior to registering for this site.");
+                                                return '<li>' + msgPart1 + '</li>';
+                                            }
+                                            return '<li>' + errorItem.user_message + '</li>';
+                                        }
                                     );
                                 }
                             )
