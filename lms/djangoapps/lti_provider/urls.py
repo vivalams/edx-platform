d299bd3865a7b2d7a0fae5b8d948d7b62b03a7e2
Manikarthik Konakalla <v-mankon@microsoft.com>
2018-09-30 17:40:48 -0700

    Adding puid in the backend for MSA (initially checked in  by satya rapelly)  (#329)
    * adding code related that adds msa PUID [written by satya rapelly] reference: https://github.com/Microsoft/edx-platform/pull/216/files#diff-fe2e2bebef5c5b8dc202c1768d240c6eL32
    
    * adding code related that adds msa PUID  missing code
    
    * adding code related that adds msa PUID  missing code
    
    * adding migration file and fixing syntax error

--- lms/djangoapps/lti_provider/urls.py
+++ lms/djangoapps/lti_provider/urls.py
@@ -8,6 +8,7 @@ from django.conf.urls import url
 from lti_provider import views
 
 urlpatterns = [
+    url(r'^users/social_auth_mapping/', 'lti_provider.views.users_social_auth_mapping', name='lti_provider_social_auth_mapping'),
     url(
         r'^courses/{course_id}/{usage_id}$'.format(
             course_id=settings.COURSE_ID_PATTERN,
