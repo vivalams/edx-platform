288c44d3f59140c1c75c6deb17ebbd608107b73d
Manikarthik Konakalla <v-mankon@microsoft.com>
2018-12-18 10:59:42 -0800

    Disable password reset endpoint based on configuration (#466)

--- lms/djangoapps/student_account/urls.py
+++ lms/djangoapps/student_account/urls.py
@@ -2,13 +2,14 @@ from django.conf import settings
 from django.conf.urls import url
 
 from student_account import views
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
 urlpatterns = [
     url(r'^finish_auth$', views.finish_auth, name='finish_auth'),
     url(r'^settings$', views.account_settings, name='account_settings'),
 ]
 
-if settings.FEATURES.get('ENABLE_COMBINED_LOGIN_REGISTRATION'):
+if settings.FEATURES.get('ENABLE_COMBINED_LOGIN_REGISTRATION') and configuration_helpers.get_value('ENABLE_RESET_PASSWORD', True):
     urlpatterns += [
         url(r'^password$', views.password_change_request_handler, name='password_change_request'),
     ]
